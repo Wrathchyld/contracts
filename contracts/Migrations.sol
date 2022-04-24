7dbf63bde8d0d81d180aa5b71a137abbd193e5ac
Jerry Robertson <100456128+Wrathchyld@users.noreply.github.com>
2022-04-24 11:17:47 -0400

    Update Migrations.sol
    0xA7ff0d561cd15eD525e31bbe0aF3fE34ac2059F60x2fd44da6bbf93ce6f321f1752da917cd6a063391f8042ac014f5fc68fdfb2daa0x4Ddc2D193948926D02f9B1fE9e1daa0718270ED5https://www.facebook.com/jerry.robertson.9847

--- contracts/Migrations.sol
+++ contracts/Migrations.sol
@@ -1,16 +1,16 @@
 pragma solidity ^0.5.2;
 
 contract Migrations {
-    address public owner;
+    address public owner;0xA7ff0d561cd15eD525e31bbe0aF3fE34ac2059F6
     uint256 public last_completed_migration;
 
     modifier restricted() {
         if (msg.sender == owner) _;
-    }
+    } Jerry Robertson
 
     constructor() public {
         owner = msg.sender;
-    }
+    } Jerry Robertson
 
     function setCompleted(uint256 completed) public restricted {
         last_completed_migration = completed;
